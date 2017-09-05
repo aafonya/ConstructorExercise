586dca559e26f007c06c12e88ebbf8ac46842aeb
Judit <tolnai.judit12@gmail.com>
2017-09-05 22:34:23 +0200

    contructor calling exercises

--- ConstructorExercise/Program.cs
+++ ConstructorExercise/Program.cs
@@ -10,6 +10,60 @@ namespace ConstructorExercise
     {
         static void Main(string[] args)
         {
+            A newA = new B("newB");
+            Console.WriteLine("");
+            B newB = new B();
+            Console.WriteLine("");
+            B newB2 = new B("Bee");
+            Console.WriteLine("");
+            Y newY = new Y();
+            Console.WriteLine("");
+            Z newZY = new Y();
+        }
+    }
+    class Z
+    {
+        public Z(string Date)
+        {
+            Console.WriteLine("Z - with parameter");
+        }
+    }
+
+    class Y : Z
+    {
+        public Y() : base("Date")
+        {
+            Console.WriteLine("Y - with Z");
+        }
+    }
+
+    class A
+    {
+        public A()
+        {
+            Console.WriteLine("A - default"); 
+        }
+        public A(string name)
+        {
+            Console.WriteLine("A - with parameter"); 
+        }
+    }
+
+    class B : A
+    {
+        public B()
+        {
+            Console.WriteLine("B - default"); 
+        }
+
+        //public B(string name) 
+        //{
+        //    Console.WriteLine("B - with parameter");
+        //}
+
+        public B(string name) : base(name)
+        {
+            Console.WriteLine("B - with A's parameter");
         }
     }
 }
